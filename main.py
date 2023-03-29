decb4755a4e0487d160528abf5eebc323b024cff
ang <57077187+constSobri@users.noreply.github.com>
2023-03-29 13:23:33 -0600

    Programa que cuenta los numeros de forma regresiva del 100 al 1

---
+++ main.py
@@ -0,0 +1,10 @@
+unoCien = 101
+
+print("Inicio del conteo regresivo del 100 al 1:")
+
+while unoCien >= 1:
+    unoCien -= 1
+    print(unoCien)
+    if unoCien == 1:
+        break
+print("Finaliza el conteo.")
